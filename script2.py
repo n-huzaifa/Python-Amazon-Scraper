01d8a0e484c309e0d539e34e2751ad9f34c2ae99
Muhammad Abu-Bakar <131898918+abubakar17-7@users.noreply.github.com>
2024-01-19 17:01:40 +0500

    Update script2.py

--- script2.py
+++ script2.py
@@ -2,13 +2,18 @@ import os
 import pyperclip
 import openpyxl
 import logging
-from time import sleep
+import json
 import datetime
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from amazoncaptcha import AmazonCaptcha
+
+with open("last_state.json", 'r') as file:
+    last_state_data = json.load(file)
 
 def setup_chrome_driver(extension_path):
     chrome_options = webdriver.ChromeOptions()
@@ -19,53 +24,113 @@ def setup_chrome_driver(extension_path):
 
 def handle_cookies(driver):
     try:
+        # Use WebDriverWait to wait for the captcha image to be present
+        image_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//div[@class='a-row a-text-center']//img")))
+        link = image_element.get_attribute('src')
+
+        # Solve the captcha
+        captcha = AmazonCaptcha.fromlink(link)
+        captcha_value = AmazonCaptcha.solve(captcha)
+
+        # Find the input field and enter the captcha value
+        input_field = driver.find_element(By.ID, "captchacharacters")
+        input_field.send_keys(captcha_value)
+
+        # Find the submit button and click it
+        button = driver.find_element(By.CLASS_NAME, "a-button-text")
+        button.click()
+    except TimeoutException:
+        pass
+    try:
         reject_button = WebDriverWait(driver, 5).until(
             EC.element_to_be_clickable((By.ID, 'sp-cc-rejectall-link')))
         reject_button.click()
     except TimeoutException:
         pass
 
-def get_excel_files(directory):
-    excel_files_by_folder = {}
+def get_category_folder(main_category_name):
+    
+    main_folder_name = main_category_name
+    
+    if main_category_name == 'Electrical Goods':
+        main_folder_name = "Category 1"
+    elif main_category_name == 'Fashion & Accessories':
+        main_folder_name = "Category 2"
+    elif main_category_name == 'Home & Garden':
+        main_folder_name = "Category 3"
+    elif main_category_name == 'Office & Business Equipment':
+        main_folder_name = "Category 4"
+    elif main_category_name == 'DIY':
+        main_folder_name = "Category 5"
+    elif main_category_name == 'Drugstore & Cosmetics':
+        main_folder_name = "Category 6"
+    elif main_category_name == 'Baby & Child':
+        main_folder_name = "Category 7"
+    elif main_category_name == 'Sport & Leisure':
+        main_folder_name = "Category 8"
+    elif main_category_name == 'Pet Supplies':
+        main_folder_name = "Category 9"
+    elif main_category_name == 'Car & Motorbike':
+        main_folder_name = "Category 10"
+    elif main_category_name == 'Books, Media & Entertainment':
+        main_folder_name = "Category 11"
+    elif main_category_name == 'Food & Beverages':
+        main_folder_name = "Category 12"
+    elif main_category_name == 'Other':
+        main_folder_name = "Category 13"
 
-    if os.path.exists(directory) and os.path.isdir(directory):
+    return main_folder_name
+
+def get_excel_files(main_category_name):
+    excel_files = []
+
+    main_folder_name = get_category_folder(main_category_name)
+    
+    if os.path.exists(main_folder_name) and os.path.isdir(main_folder_name):
+        excel_files = []
+
         # Iterate through all subdirectories and files in the given directory
-        for root, dirs, files in os.walk(directory):
+        for root, dirs, files in os.walk(main_folder_name):
             # Check if the current directory contains the specified text
             if "skript 1" in root.lower():
-                for file in files:
-                    # Check if the file has a .xls or .xlsx extension
-                    if file.lower().endswith(('.xls', '.xlsx')):
+                
+                excel_files_in_folder = [file for file in files if file.lower().endswith(('.xls', '.xlsx'))]
+                    
+                if excel_files_in_folder:
                         # Create the full path to the Excel file
-                        file_path = os.path.join(root, file)
-                        
-                        # Update the dictionary with the latest file for each folder
-                        folder_key = os.path.dirname(file_path)
-                        if folder_key not in excel_files_by_folder or os.path.getmtime(file_path) > os.path.getmtime(excel_files_by_folder[folder_key]):
-                            excel_files_by_folder[folder_key] = file_path
+                        latest_file = max(excel_files_in_folder, key=lambda x: os.path.getctime(os.path.join(root, x)))
+                        file_path = os.path.join(root, latest_file)
+                        excel_files.append(file_path)
 
-    # Return a list of the latest Excel files from each folder
-    return list(excel_files_by_folder.values())
+    return excel_files
 
-def load_amazon_urls(excel_file_path):
+def load_amazon_urls(excel_files):
     try:
-        wb = openpyxl.load_workbook(excel_file_path)
-        sheet = wb.active
-        amazon_urls = [tuple(sheet.cell(row=cell.row, column=col).value for col in range(1, sheet.max_column + 1)) for cell in sheet['A'] if cell.value and cell.row > 1]
+        all_amazon_urls = []
+
+        for excel_file_path in excel_files:
+            wb = openpyxl.load_workbook(excel_file_path)
+            sheet = wb.active
+            amazon_urls = [tuple(sheet.cell(row=cell.row, column=col).value for col in range(1, sheet.max_column + 1)) for cell in sheet['A'] if cell.value and cell.row > 1]
+            all_amazon_urls.extend(amazon_urls)
+            
         logging.info(f"Amazon URLs loaded from: {excel_file_path}")
-        return amazon_urls
+        all_amazon_urls_sorted = sorted(all_amazon_urls, key=lambda x: x[2])
+        return all_amazon_urls_sorted
     
     except Exception as e:
         logging.error(f"Error loading Amazon URLs from {excel_file_path}: {e}")
         raise
     
-def create_or_load_workbook(directory):
+def create_or_load_workbook(category):
     
     try:
-        folder_path = os.path.join(directory, f'6. Skript 2')
+        
+        main_folder_name = get_category_folder(category)
+        folder_path = os.path.join(main_folder_name, f'6. Skript 2')
         os.makedirs(folder_path, exist_ok=True)
 
-        excel_file_path = os.path.join(folder_path, f'./Script2_Germany_{directory}_{datetime.datetime.now().strftime("%Y%m%d")}.xlsx')
+        excel_file_path = os.path.join(folder_path, f'./Script2_Germany_{category}_{datetime.datetime.now().strftime("%Y%m%d")}.xlsx')
 
         if os.path.isfile(excel_file_path):
             workbook = openpyxl.load_workbook(excel_file_path)
@@ -94,43 +159,36 @@ def extract_asins_and_category(driver, url, worksheet, last_row, Category_Level)
     sleep(3)
     handle_cookies(driver)
 
-    max_attempts = 3  # You can adjust the maximum number of retry attempts
+    try:
+        asin_extractor = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'azASINExtractorDropDown')))
+        asin_extractor.click()
 
-    for attempt in range(max_attempts):
-        try:
-            asin_extractor = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'azASINExtractorDropDown')))
-            asin_extractor.click()
+        all_asins_option = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@class, 'header') and contains(text(), 'All ASINs')]")))
+        all_asins_option.click()
 
-            all_asins_option = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@class, 'header') and contains(text(), 'All ASINs')]")))
-            all_asins_option.click()
+        sleep(3)
+        captured_asins = pyperclip.paste()
+        asins_list = captured_asins.split("\n")
 
-            sleep(3)
-            captured_asins = pyperclip.paste()
-            asins_list = captured_asins.split("\n")
+        if not asins_list:
+            raise ValueError("ASINs list is empty.")
 
-            if not asins_list:
-                raise ValueError("ASINs list is empty. Retrying...")
+        category_name = driver.find_element(By.XPATH, "//h1[contains(@class, 'a-size-large')]").text
+        category_name = category_name.replace("Best Sellers in ", "")
 
-            category_name = driver.find_element(By.XPATH, "//h1[contains(@class, 'a-size-large')]").text
-            category_name = category_name.replace("Best Sellers in ", "")
+        for value in asins_list:
+            worksheet.cell(row=last_row, column=1).value = value
+            worksheet.cell(row=last_row, column=2).value = category_name
+            worksheet.cell(row=last_row, column=3).value = Category_Level
+            worksheet.cell(row=last_row, column=4).value = datetime.datetime.now().strftime("%Y-%m-%d")
+            last_row += 1
+        logging.info(f"ASINs extracted from: {category_name}")
+        return last_row
+        # Break out of the loop if extraction is successful
 
-            for value in asins_list:
-                worksheet.cell(row=last_row, column=1).value = value
-                worksheet.cell(row=last_row, column=2).value = category_name
-                worksheet.cell(row=last_row, column=3).value = Category_Level
-                worksheet.cell(row=last_row, column=4).value = datetime.datetime.now().strftime("%Y-%m-%d")
-                last_row += 1
-            logging.info(f"ASINs extracted from: {category_name}")
-            return last_row
-            # Break out of the loop if extraction is successful
-
-        except Exception as e:
-            logging.error("Error:", e)
-            if attempt < max_attempts - 1:
-                logging.info(f"Retry attempt {attempt + 1}/{max_attempts}")
-            else:
-                logging.info("Max retry attempts reached. Exiting.")
-                return last_row
+    except Exception as e:
+        logging.warning(f"ASINs list is empty")
+        return last_row
 
 def remove_duplicate_asins(worksheet):
     unique_asins = set()
@@ -155,33 +213,26 @@ def save_workbook(workbook, excel_file_path):
         logging.error(f"Error saving workbook: {e}")
         raise
 
-def main(driver, directory = None):
-
-    logging.basicConfig(filename=f'logfile_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.txt ', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+def main(driver, category):
 
     try:
-        excel_files = get_excel_files(directory)
-        workbook, asin_excel_file_path, last_row = create_or_load_workbook(directory)
-                        
-        for excel_file_path in excel_files:
+        excel_files = get_excel_files(category)
+        workbook, asin_excel_file_path, last_row = create_or_load_workbook(category)
+                
+        amazon_urls = load_amazon_urls(excel_files)
+
+        for url in amazon_urls:
             try:
-                amazon_urls = load_amazon_urls(excel_file_path)
+                Category_URL, Category_Level = url[1], url[2]
 
-                for url in amazon_urls:
-                    try:
-                        Category_URL, Category_Level = url[1], url[2]
+                worksheet = workbook.active
 
-                        worksheet = workbook.active
+                last_row = extract_asins_and_category(driver, Category_URL, worksheet, last_row, Category_Level)
 
-                        last_row = extract_asins_and_category(driver, Category_URL, worksheet, last_row, Category_Level)
-
-                        save_workbook(workbook, asin_excel_file_path)
-                    
-                    except Exception as e:
-                        logging.error(f"Error processing URL {url}: {e}")
-
+                save_workbook(workbook, asin_excel_file_path)
+            
             except Exception as e:
-                logging.error(f"Error processing Excel file {excel_file_path}: {e}")
+                logging.error(f"Error processing URL {Category_URL}: {e}")
         
         remove_duplicate_asins(worksheet)
         save_workbook(workbook, asin_excel_file_path)
@@ -194,7 +245,8 @@ def main(driver, directory = None):
         logging.error(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
+    logging.basicConfig(filename=f'Log Files\\logfile_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.txt ', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     extension_path = './amazoncrxextension.crx'
     driver = setup_chrome_driver(extension_path)
     handle_cookies(driver)
-    main(driver, "Adapter")
+    main(driver, "Electrical Goods")
