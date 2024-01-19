38b3c1fe8c1e79d557f35b437ea2547d55aefdc1
Muhammad Abu-Bakar <131898918+abubakar17-7@users.noreply.github.com>
2024-01-19 16:59:59 +0500

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,20 +1,24 @@
 import os
 import openpyxl
 import logging
-from time import sleep
-from datetime import datetime
+import json
+import datetime
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from amazoncaptcha import AmazonCaptcha
+from openpyxl.utils import get_column_letter
 
-all_urls = [[ "https://www.amazon.de/-/en/gp/bestsellers/computers/430049031/ref=zg_bs_nav_computers_2_427958031" , 2, "Adapters"],
-             ["https://www.amazon.de/-/en/gp/new-releases/computers/430049031/ref=zg_bsnr_nav_computers_2_427958031" , 2, "Adapters"],
-             ["https://www.amazon.de/-/en/gp/most-wished-for/computers/430049031/ref=zg_mw_nav_computers_2_427958031" , 2, "Adapters"],
-             ["https://www.amazon.de/-/en/gp/most-gifted/computers/430049031/ref=zg_mg_nav_computers_2_427958031" , 2, "Adapters"]
-             ]
+with open("last_state.json", 'r') as file:
+    last_state_data = json.load(file)
+
+with open("data.json", 'r') as file:
+    data = json.load(file)
+
+all_urls = data["all_urls"]
 
 def setup_chrome_driver(extension_path):
     chrome_options = webdriver.ChromeOptions()
@@ -25,32 +29,69 @@ def setup_chrome_driver(extension_path):
 
 def handle_cookies(driver):
     try:
+        # Use WebDriverWait to wait for the captcha image to be present
+        image_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//div[@class='a-row a-text-center']//img")))
+        link = image_element.get_attribute('src')
+
+        # Solve the captcha
+        captcha = AmazonCaptcha.fromlink(link)
+        captcha_value = AmazonCaptcha.solve(captcha)
+
+        # Find the input field and enter the captcha value
+        input_field = driver.find_element(By.ID, "captchacharacters")
+        input_field.send_keys(captcha_value)
+
+        # Find the submit button and click it
+        button = driver.find_element(By.CLASS_NAME, "a-button-text")
+        button.click()
+    except TimeoutException:
+        pass
+    try:
         reject_button = WebDriverWait(driver, 5).until(
             EC.element_to_be_clickable((By.ID, 'sp-cc-rejectall-link')))
         reject_button.click()
     except TimeoutException:
         pass
 
-def load_or_create_workbook(Category_Name, Category_Type, excel_file_name):
+def get_category_folder(main_category_name):
     
-    category_folder_name = Category_Name
-    os.makedirs(category_folder_name, exist_ok=True)
-
-    if Category_Type == 'Hot New Releases':
-        category_type_folder_path = os.path.join(Category_Name, f'1. Skript 1 {Category_Type}')
-    elif Category_Type == 'Best Sellers':
-        category_type_folder_path = os.path.join(Category_Name, f'2. Skript 1 {Category_Type}')
-    elif Category_Type == 'Movers & Shakers':
-        category_type_folder_path = os.path.join(Category_Name, f'3. Skript 1 {Category_Type}')
-    elif Category_Type == 'Most Wished For':
-        category_type_folder_path = os.path.join(Category_Name, f'4. Skript 1 {Category_Type}')
-    elif Category_Type == 'Most Gifted':
-        category_type_folder_path = os.path.join(Category_Name, f'5. Skript 1 {Category_Type}')
+    main_folder_name = main_category_name
     
-    os.makedirs(category_type_folder_path, exist_ok=True)
+    if main_category_name == 'Electrical Goods':
+        main_folder_name = "Category 1"
+    elif main_category_name == 'Fashion & Accessories':
+        main_folder_name = "Category 2"
+    elif main_category_name == 'Home & Garden':
+        main_folder_name = "Category 3"
+    elif main_category_name == 'Office & Business Equipment':
+        main_folder_name = "Category 4"
+    elif main_category_name == 'DIY':
+        main_folder_name = "Category 5"
+    elif main_category_name == 'Drugstore & Cosmetics':
+        main_folder_name = "Category 6"
+    elif main_category_name == 'Baby & Child':
+        main_folder_name = "Category 7"
+    elif main_category_name == 'Sport & Leisure':
+        main_folder_name = "Category 8"
+    elif main_category_name == 'Pet Supplies':
+        main_folder_name = "Category 9"
+    elif main_category_name == 'Car & Motorbike':
+        main_folder_name = "Category 10"
+    elif main_category_name == 'Books, Media & Entertainment':
+        main_folder_name = "Category 11"
+    elif main_category_name == 'Food & Beverages':
+        main_folder_name = "Category 12"
+    elif main_category_name == 'Other':
+        main_folder_name = "Category 13"
 
-    excel_file_path = os.path.join(category_type_folder_path, excel_file_name)
+    return main_folder_name
 
+def load_or_create_workbook(main_category_name, category_type, category_type_no, excel_file_name):
+
+    main_folder_name = get_category_folder(main_category_name)
+
+    excel_file_path = os.path.join(main_folder_name, f'{category_type_no}. Skript 1 {category_type}', excel_file_name)
+    print(excel_file_path)
     try:
         workbook = openpyxl.load_workbook(excel_file_path)
         worksheet = workbook.active
@@ -64,22 +105,18 @@ def load_or_create_workbook(Category_Name, Category_Type, excel_file_name):
 
 def scrape_and_write(driver, worksheet, level, url, row, main_category_type):
     logging.info(f"Scraping and writing data for level {level} from URL: {url}")
-
-    for current_level in range(level, 4):
-        level_urls = scrape_categories(driver, url)
-        for level_url in level_urls:
-            if level_url['url']:
-                worksheet.cell(row=row, column=1).value = level_url['name']
-                worksheet.cell(row=row, column=2).value = level_url['url']
-                if str(current_level) == "0":
-                    worksheet.cell(row=row, column=3).value = "100"
-                else:
-                    worksheet.cell(row=row, column=3).value = str(current_level)
-                worksheet.cell(row=row, column=4).value = main_category_type
-                row += 1
-
-                if current_level < 4:
-                    row = scrape_and_write(driver, worksheet, current_level + 1, level_url['url'], row, main_category_type)
+    
+    current_level = level
+    level_urls = scrape_categories(driver, url)
+    for level_url in level_urls:
+        worksheet.cell(row=row, column=1).value = level_url['name']
+        worksheet.cell(row=row, column=2).value = level_url['url']
+        worksheet.cell(row=row, column=3).value = str(current_level + 1)
+        worksheet.cell(row=row, column=4).value = main_category_type
+        
+        row += 1
+        if current_level + 1 < 3:
+            row = scrape_and_write(driver, worksheet, current_level + 1, level_url['url'], row, main_category_type)
 
     logging.info(f"Scraping and writing data for level {level} completed.")
     return row
@@ -107,7 +144,8 @@ def scrape_categories(driver, url):
                     span_tag = item_div.find('span')
 
                     if span_tag:
-                        categories.append({'name': span_tag.text.strip(), 'url': ''})
+                        categories = []
+                        break
 
                     if anchor_tag:
                         category_name = anchor_tag.text.strip()
@@ -136,53 +174,62 @@ def remove_duplicate_categories(worksheet):
 
     logging.info("Duplicate categories removed successfully.")
 
-def save_workbook(workbook, excel_file_path):
+def save_workbook(workbook, worksheet, excel_file_path):
     try:
         workbook.save(excel_file_path)
+        worksheet.sort(column=get_column_letter(3))
+        workbook.save(excel_file_path)
+        workbook.close()
         logging.info(f"Workbook saved successfully: {excel_file_path}")
     except Exception as e:
         logging.error(f"An error occurred while saving workbook {excel_file_path}: {str(e)}")
 
-def main(driver, url_data):
+def main(driver, all_urls):
     
     try:
-        url = url_data[0]
-        level = url_data[1]
-        category_name = url_data[2]
+        for url_data in all_urls:
+            url = url_data[0]
+            level = url_data[1]
+            # 1 - Electrical Goods - Camera & Photo - Hot New Releases
+            category_data = url_data[2].split("-")
 
-        driver.get(url)
-        sleep(5)
-        handle_cookies(driver)
+            driver.get(url)
+            handle_cookies(driver)
 
-        soup = BeautifulSoup(driver.page_source, "lxml")
-        page_title = soup.title.text.strip()
-        main_category_name = category_name
-        main_category_type = page_title[10:page_title.find(":")].strip()
+            main_category_name = category_data[1].strip()
+            category_name = category_data[2].strip()
+            category_type = category_data[3].strip()
+            category_type_no = category_data[0].strip()
 
-        excel_file_name = f'./Script1_Germany_{main_category_name}_{datetime.now().strftime("%Y%m%d")}.xlsx'
+            excel_file_name = f'./Script1_Germany_{main_category_name}_{datetime.datetime.now().strftime("%Y%m%d")}.xlsx'
 
-        excel_file_path, workbook, worksheet = load_or_create_workbook(main_category_name, main_category_type, excel_file_name)
+            excel_file_path, workbook, worksheet = load_or_create_workbook(main_category_name, category_type, category_type_no, excel_file_name)
 
-        worksheet.cell(row=1, column=1).value = "Category Name"
-        worksheet.cell(row=1, column=2).value = "Category URL"
-        worksheet.cell(row=1, column=3).value = "Category Level"
-        worksheet.cell(row=1, column=4).value = "Category Type"
+            worksheet.cell(row=1, column=1).value = "Category Name"
+            worksheet.cell(row=1, column=2).value = "Category URL"
+            worksheet.cell(row=1, column=3).value = "Category Level"
+            worksheet.cell(row=1, column=4).value = "Category Type"
+            worksheet.cell(row=2, column=1).value = category_name
+            worksheet.cell(row=2, column=2).value = url
+            worksheet.cell(row=2, column=3).value = "100" if level == 0 else str(level)
+            worksheet.cell(row=2, column=4).value = category_type
+            workbook.save(excel_file_path)
 
-        row = 2
-        row = scrape_and_write(driver, worksheet, level, url, row, main_category_type)
+            row = 3
+            row = scrape_and_write(driver, worksheet, level, url, row, category_type)
 
-        remove_duplicate_categories(worksheet)
-        save_workbook(workbook, excel_file_path)
+            remove_duplicate_categories(worksheet)
+            save_workbook(workbook, worksheet, excel_file_path)
 
     except Exception as e:
         logging.error(f"An error occurred while scraping URL {url}: {str(e)}")
 
-    logging.info(f"All URLs for {main_category_type} processed successfully.")
-
 if __name__ == "__main__":
+
+    logging.basicConfig(filename=f'Log Files\\logfile_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.txt ', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     base_url = 'https://www.amazon.de/'
     extension_path = './amazoncrxextension.crx'
     driver = setup_chrome_driver(extension_path)
     handle_cookies(driver)
-    for url_data in all_urls:
-        main(driver, url_data)
+    
+    main(driver, all_urls)
